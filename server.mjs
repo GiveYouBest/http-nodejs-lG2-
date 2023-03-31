67a0064acbd4e8a2cb32ffc18352e717fe2d4ae5
GiveYouBest <49054215+GiveYouBest@users.noreply.github.com>
2023-03-31 14:36:02 +0800

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -2,5 +2,6 @@ import { createServer } from 'http';
 
 createServer((req, res) => {
   res.write('Hello World!');
+  res.send({msg:'success',data:[{name:'233'}]})
   res.end();
 }).listen(process.env.PORT);
